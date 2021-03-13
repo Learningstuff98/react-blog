1064b91af2ab1c5d2821d6becdad1b452ff5f35e
Andy Strube <andystrube64@gmail.com>
2021-03-12 21:00:48 -0800

    Added some code

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+
 function Home() {
-  return <div>
-    <h1>HomePage</h1>
-  </div>  
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
+
+  return <div className="home">
+    {blogs.map((blog) => {
+      return <div className="blog-preview" key={blog.id}>
+        <div>
+          {blog.title}
+        </div>
+        Author: {blog.author}
+      </div>
+    })}
+  </div>
 }
 
 export default Home;
